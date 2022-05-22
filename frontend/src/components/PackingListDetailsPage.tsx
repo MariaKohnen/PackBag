1226af9b8d8767846b4d29b8cc1ec0fdb18a75e4
Maria Kohnen <maria.kohnen@gmail.com>
2022-05-22 13:11:36 +0200

    #15 frontend: shows destination on detailsPage

--- frontend/src/components/PackingListDetailsPage.tsx
+++ frontend/src/components/PackingListDetailsPage.tsx
@@ -1,6 +1,22 @@
+import useDetailedPackingList from "../hooks/useDetailedPackingList";
+import {useEffect} from "react";
+import {useParams} from "react-router-dom";
 
 export default function PackingListDetailsPage() {
+
+    const {detailedPackingList, getDetailedPackingListById} = useDetailedPackingList()
+    const {id} = useParams()
+
+    useEffect(() => {
+        if(id) {
+            getDetailedPackingListById(id)
+        }
+    },[id])
+
     return (
-        <div><h2>Details</h2></div>
+        <div>
+            <h2>Details</h2>
+            {detailedPackingList && <p>{detailedPackingList.destination}</p>}
+        </div>
     )
 }
