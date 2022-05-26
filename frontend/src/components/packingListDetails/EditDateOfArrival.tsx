c8df98f1839b9b595482bcb9995654cca6c2acfb
Maria Kohnen <maria.kohnen@gmail.com>
2022-05-26 12:34:07 +0200

    #8 added date picker with wrong format, changed type from string to Date

--- frontend/src/components/packingListDetails/EditDateOfArrival.tsx
+++ frontend/src/components/packingListDetails/EditDateOfArrival.tsx
@@ -1,20 +1,29 @@
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {Stack} from "@mui/material";
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
 type EditDateOfArrivalProps = {
-    newDateOfArrival : string
-    setNewDateOfArrival: (newDateOfArrival: string) => void
+    newDateOfArrival : Date
+    setNewDateOfArrival: (newDateOfArrival: Date) => void
 }
 
 export default function EditDateOfArrival({newDateOfArrival, setNewDateOfArrival}: EditDateOfArrivalProps) {
     return (
         <div className="to-change">
             <p>When are you planning to go?</p>
-            <input
-                className="input-field"
-                type={"text"}
-                value={newDateOfArrival}
-                placeholder={"choose a date"}
-                onChange={event => setNewDateOfArrival(event.target.value)}
-            />
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Stack spacing={3}>
+                    <MobileDatePicker
+                        label="Date desktop"
+                        inputFormat="yyyy-MM-dd"
+                        value={newDateOfArrival}
+                        onChange={event => event && setNewDateOfArrival(event)}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </Stack>
+            </LocalizationProvider>
         </div>
     )
 }
