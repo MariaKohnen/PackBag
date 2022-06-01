e75f10b15592c9ff0093cde170c47e587ca072be
Maria Kohnen <maria.kohnen@gmail.com>
2022-06-01 09:38:31 +0200

    #14 frontend: styling

--- frontend/src/components/packingItemDetails/AddItemToPackingList.tsx
+++ frontend/src/components/packingItemDetails/AddItemToPackingList.tsx
@@ -1,6 +1,9 @@
-import {FormEvent, useState} from "react";
+import React, {FormEvent, useState} from "react";
 import {toast} from "react-toastify";
 import {PackingItem} from "../../model/PackingItem";
+import "./styling/AddItemToPackingList.css";
+import {AiOutlinePlus} from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 type AddItemToPackingListProps = {
     addItemToPackingList : (id: string, newPackingItem: Omit<PackingItem, "id">) => void
@@ -26,16 +29,17 @@ export default function AddItemToPackingList({addItemToPackingList, id}: AddItem
 
     return (
         <div>
+            <IconContext.Provider value={{color: '#a87f4f'}}>
             <form className="add-packing-item" onSubmit={getOnSubmit}>
                 <input className="text-field"
                        type="name"
-                       placeholder="add a new item to your list?"
+                       placeholder="add a new item to your list"
                        value={newName}
                        onChange={event => setNewName(event.target.value)}/>
-                <input className="button"
-                       type="submit"
-                       value={"add item"}/>
+                <button className="button"
+                       type="submit"><AiOutlinePlus/></button>
             </form>
+            </IconContext.Provider>
         </div>
     )
 }
