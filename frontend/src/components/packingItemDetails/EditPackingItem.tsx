547f2b649fdec2feb7aed5650627dec0902c6f16
Maria Kohnen <maria.kohnen@gmail.com>
2022-06-02 17:12:40 +0200

    #17 frontend: validate user input to be not null

--- frontend/src/components/packingItemDetails/EditPackingItem.tsx
+++ frontend/src/components/packingItemDetails/EditPackingItem.tsx
@@ -1,6 +1,7 @@
 import {FormEvent, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import {PackingItem} from "../../model/PackingItem";
+import {toast} from "react-toastify";
 
 type EditPackingItemProps = {
     updateItemAndGetUpdatedList: (id: string, itemId: string, updatedPackingItem: Omit<PackingItem, "id">) => void
@@ -11,16 +12,19 @@ export default function EditPackingItem({updateItemAndGetUpdatedList, id}: EditP
 
     const {itemId} = useParams()
     const navigate = useNavigate()
-    const [newName, setNewName] = useState<string>("new Name")
+    const [newName, setNewName] = useState<string>("")
 
     const handleClick  = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
+        if (!newName) {
+            toast.error("Please enter a name or return!")
+            return
+        }
         const editedItemDto: Omit<PackingItem, "id"> = {
             name: newName
         }
-        itemId && updateItemAndGetUpdatedList(id, itemId, editedItemDto)
-        navigate(-1)
-    }
+            itemId && updateItemAndGetUpdatedList(id, itemId, editedItemDto)
+            navigate(-1)}
 
     return (
         <form onSubmit={handleClick}>
@@ -28,7 +32,7 @@ export default function EditPackingItem({updateItemAndGetUpdatedList, id}: EditP
                 className="input-field"
                 type={"text"}
                 value={newName}
-                placeholder="nothing here"
+                placeholder="change name of item"
                 onChange={event => setNewName(event.target.value)}
             />
         <button type={"submit"}>save changes</button>
