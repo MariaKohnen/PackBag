4f2b812f93632bc47c578711355b38cf53c8f89c
Maria Kohnen <maria.kohnen@gmail.com>
2022-06-03 16:07:47 +0200

    #72 split test classes into packingItem-tests and packingList-tests

--- backend/src/test/java/com/github/mariakohnen/packbag/backend/controller/PackingListControllerTest.java
+++ backend/src/test/java/com/github/mariakohnen/packbag/backend/controller/PackingListControllerTest.java
@@ -1,36 +1,29 @@
 package com.github.mariakohnen.packbag.backend.controller;
 
-import com.github.mariakohnen.packbag.backend.dto.NewPackingItemDto;
 import com.github.mariakohnen.packbag.backend.dto.NewPackingListDto;
 import com.github.mariakohnen.packbag.backend.dto.UpdatePackingListDto;
 import com.github.mariakohnen.packbag.backend.model.PackingItem;
 import com.github.mariakohnen.packbag.backend.model.PackingList;
 import com.github.mariakohnen.packbag.backend.repository.PackingListRepository;
-import com.github.mariakohnen.packbag.backend.service.IdService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PackingListControllerTest {
 
     @Autowired
-    private WebTestClient webTestClient;
+    WebTestClient webTestClient;
 
     @Autowired
-    private PackingListRepository packingListRepository;
-
-    @MockBean
-    private IdService idService;
+    PackingListRepository packingListRepository;
 
     @BeforeEach
     public void cleanUp() {
@@ -70,16 +63,7 @@ class PackingListControllerTest {
                             .build()))
             .build();
 
-    NewPackingItemDto newPackingItemDto = NewPackingItemDto.builder()
-            .name("swimwear")
-            .build();
-
-    NewPackingItemDto newPackingItemDto2 = NewPackingItemDto.builder()
-            .name("passport")
-            .build();
-
     String listId = "1";
-    String itemId = "01";
     String invalidId = "123";
 
     @Test
@@ -233,47 +217,6 @@ class PackingListControllerTest {
     }
 
     @Test
-    void addPackingItemToPackingList_whenNameIsGiveAndActualListIsNull_shouldReturnPackingList() {
-        //GIVEN
-        PackingList packingList = PackingList.builder()
-                .id("1")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .destination("Kyoto")
-                .packingItemList(List.of())
-                .build();
-        packingListRepository.insert(packingList);
-        //WHEN
-        assertNotNull(packingList);
-        when(idService.generateId()).thenReturn("01");
-        PackingList actual = webTestClient.put()
-                .uri("/api/packinglists/" + listId + "/packingitems")
-                .bodyValue(newPackingItemDto2)
-                .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBody(PackingList.class)
-                .returnResult()
-                .getResponseBody();
-        //THEN
-        PackingList expected = packingListWithOneItem;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void addPackingItemToPackingList_whenNameIsNotGiven_shouldThrowNoSuchElementException() {
-        //GIVEN
-        packingListRepository.insert(packingListWithOneItem);
-        //WHEN
-        assertNotNull(packingListWithOneItem);
-        NewPackingItemDto emptyItem = NewPackingItemDto.builder()
-                .build();
-        webTestClient.put()
-                .uri("/api/packinglists/" + listId + "/packingitems")
-                .bodyValue(emptyItem)
-                .exchange()
-                .expectStatus().isEqualTo(400);
-    }
-
-    @Test
     void deletePackingListByID_whenIdIsValid() {
         //GIVEN
         packingListRepository.insert(packingListWithOneItem);
@@ -285,110 +228,5 @@ class PackingListControllerTest {
                 .expectStatus().is2xxSuccessful();
     }
 
-    @Test
-    void deleteItemFromPackingList_whenIdOfListAndItemAreValid() {
-        //GIVEN
-        packingListRepository.insert(packingListWithTwoItems);
-        String itemToDelete = "02";
-        //WHEN
-        assertNotNull(packingListWithTwoItems);
-        PackingList actual = webTestClient.delete()
-                .uri("/api/packinglists/" + listId + "/packingitems/" + itemToDelete)
-                .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBody(PackingList.class)
-                .returnResult()
-                .getResponseBody();
-        //THEN
-        PackingList excepted = packingListWithOneItem;
-        assertEquals(excepted, actual);
-    }
 
-    @Test
-    void deleteItemFromPackingList_whenIdOfListIsNotValid_ShouldThrowNoSuchElementException() {
-        //GIVEN
-        packingListRepository.insert(packingListWithTwoItems);
-        String itemToDelete = "02";
-        String wrongListId = "2";
-        //WHEN //THEN
-        assertNotNull(packingListWithTwoItems);
-        webTestClient.delete()
-                .uri("/api/packinglists/" + wrongListId + "/packingitems/" + itemToDelete)
-                .exchange()
-                .expectStatus().isEqualTo(404);
-    }
-
-    @Test
-    void deleteItemFromPackingList_whenListHasNoItems_ShouldThrowNoSuchElementException() {//GIVEN
-        PackingList packingListWithNoItems = PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .build();
-        packingListRepository.insert(packingListWithNoItems);
-        //WHEN //THEN
-        assertNotNull(packingListWithNoItems);
-        webTestClient.delete()
-                .uri("/api/packinglists/" + listId + "/packingitems/" + itemId)
-                .exchange()
-                .expectStatus().isEqualTo(404);
-    }
-
-    @Test
-    void updateExistingPackingItemOfList_whenIdOfListAndItemIsValid_shouldReturnUpdatedList() {
-        //GIVEN
-        packingListRepository.insert(packingListWithTwoItems);
-        //WHEN
-        assertNotNull(packingListWithTwoItems);
-        PackingList actual = webTestClient.put()
-                .uri("/api/packinglists/" + listId + "/packingitems/" + itemId)
-                .bodyValue(newPackingItemDto)
-                .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBody(PackingList.class)
-                .returnResult()
-                .getResponseBody();
-        //THEN
-        PackingList excepted = PackingList.builder()
-                .id("1")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .destination("Kyoto")
-                .packingItemList(List.of(PackingItem.builder()
-                                .id("01")
-                                .name("swimwear")
-                                .build(),
-                        PackingItem.builder()
-                                .id("02")
-                                .name("swimwear")
-                                .build()))
-                .build();
-        assertEquals(excepted, actual);
-    }
-
-    @Test
-    void updateExistingPackingItemOfList_whenIdIsNotValid_shouldThrowNoSuchElementException() {
-        //GIVEN
-        packingListRepository.insert(packingListWithTwoItems);
-        String invalidItemId = "1";
-        //WHEN //THEN
-        assertNotNull(packingListWithTwoItems);
-        webTestClient.put()
-                .uri("/api/packinglists/" + listId + "/packingitems/" + invalidItemId)
-                .bodyValue(newPackingItemDto)
-                .exchange()
-                .expectStatus().isEqualTo(404);
-    }
-
-    @Test
-    void updateExistingPackingItemOfList_whenNameOfItemIsNotGiven_shouldThrowIllegalArgumentException() {
-        //GIVEN
-        NewPackingItemDto emptyItem = NewPackingItemDto.builder()
-                .build();
-        //WHEN //THEN
-        webTestClient.put()
-                .uri("/api/packinglists/" + listId + "/packingitems/" + itemId)
-                .bodyValue(emptyItem)
-                .exchange()
-                .expectStatus().isEqualTo(400);
-    }
 }
--- backend/src/test/java/com/github/mariakohnen/packbag/backend/service/PackingListServiceTest.java
+++ backend/src/test/java/com/github/mariakohnen/packbag/backend/service/PackingListServiceTest.java
@@ -1,6 +1,5 @@
 package com.github.mariakohnen.packbag.backend.service;
 
-import com.github.mariakohnen.packbag.backend.dto.NewPackingItemDto;
 import com.github.mariakohnen.packbag.backend.dto.NewPackingListDto;
 import com.github.mariakohnen.packbag.backend.dto.UpdatePackingListDto;
 import com.github.mariakohnen.packbag.backend.model.PackingItem;
@@ -18,10 +17,10 @@ import static org.mockito.Mockito.*;
 
 class PackingListServiceTest {
 
-    private final PackingListRepository packingListRepository = mock(PackingListRepository.class);
-    private final IdService idService = mock(IdService.class);
+    final PackingListRepository packingListRepository = mock(PackingListRepository.class);
+    final IdService idService = mock(IdService.class);
 
-    private final PackingListService packingListService = new PackingListService(packingListRepository, idService);
+    final PackingListService packingListService = new PackingListService(packingListRepository, idService);
 
     NewPackingListDto newPackingListDto = NewPackingListDto.builder()
             .destination("Kyoto")
@@ -56,16 +55,7 @@ class PackingListServiceTest {
                             .build()))
             .build();
 
-    NewPackingItemDto newPackingItemDto = NewPackingItemDto.builder()
-            .name("swimwear")
-            .build();
-
-    NewPackingItemDto newPackingItemDto2 = NewPackingItemDto.builder()
-            .name("passport")
-            .build();
-
     String listId = "1";
-    String itemId = "01";
     String invalidId = "123";
 
     @Test
@@ -214,231 +204,5 @@ class PackingListServiceTest {
         verify(packingListRepository).deleteById("1");
     }
 
-    @Test
-    void addNewPackingItem_whenNameIsGivenAndActualListIsNotNull_ShouldReturnUpdatedPackingListWithAllItems() {
-        //GIVEN
-        when(idService.generateId()).thenReturn("02");
 
-        when(packingListRepository.findById(listId)).thenReturn(Optional.ofNullable(packingListWithOneItem));
-
-        PackingList updatedPackingList = PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(PackingItem.builder()
-                                .id("01")
-                                .name("passport")
-                                .build(),
-                        PackingItem.builder()
-                                .id("02")
-                                .name("swimwear")
-                                .build()))
-                .build();
-
-        when(packingListRepository.save(updatedPackingList)).thenReturn(packingListWithTwoItems);
-        //WHEN
-        PackingList actual = packingListService.addNewPackingItem(listId, newPackingItemDto);
-        //THEN
-        PackingList expected = PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(PackingItem.builder()
-                                .id("01")
-                                .name("passport")
-                                .build(),
-                        PackingItem.builder()
-                                .id("02")
-                                .name("swimwear")
-                                .build()))
-                .build();
-        assertEquals(expected, actual);
-        verify(packingListRepository).save(updatedPackingList);
-        verify(idService).generateId();
-    }
-
-    @Test
-    void addNewPackingItem_whenNameIsGivenAndActualListIsNull_ShouldReturnUpdatedPackingListWithAllItems() {
-        //GIVEN
-        when(idService.generateId()).thenReturn("01");
-
-        when(packingListRepository.findById(listId)).thenReturn(Optional.ofNullable(
-                PackingList.builder()
-                        .id("1")
-                        .destination("Kyoto")
-                        .dateOfArrival(LocalDate.parse("2022-09-03"))
-                        .build()));
-
-        when(packingListRepository.save(packingListWithOneItem)).thenReturn(PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(
-                        PackingItem.builder()
-                                .id("01")
-                                .name("passport")
-                                .build()))
-                .build());
-        //WHEN
-        PackingList actual = packingListService.addNewPackingItem(listId, newPackingItemDto2);
-        //THEN
-        PackingList expected = PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(
-                        PackingItem.builder()
-                                .id("01")
-                                .name("passport")
-                                .build()))
-                .build();
-        assertEquals(expected, actual);
-        verify(packingListRepository).save(packingListWithOneItem);
-        verify(idService).generateId();
-    }
-
-    @Test
-    void addNewPackingItem_whenNameIsNull_ShouldThrowIllegalArgumentException() {
-        //GIVEN
-        NewPackingItemDto emptyItem = NewPackingItemDto.builder()
-                .build();
-        when(packingListRepository.findById(listId)).thenReturn(Optional.ofNullable(packingListWithOneItem));
-        //WHEN//THEN
-        assertThrows(IllegalArgumentException.class, () -> packingListService.addNewPackingItem(listId, emptyItem));
-    }
-
-    @Test
-    void generateNewItem_whenNameIsGiven_ShouldReturnPackingItem() {
-        //GIVEN
-        when(idService.generateId()).thenReturn("01");
-        //WHEN
-        PackingItem actual = packingListService.generateNewItem(newPackingItemDto);
-        //THEN
-        PackingItem expected = PackingItem.builder()
-                .id("01")
-                .name("swimwear")
-                .build();
-        assertEquals(expected, actual);
-        verify(idService).generateId();
-    }
-
-    @Test
-    void generateNewItem_whenNameIsNotGiven_ShouldThrowIllegalArgumentException() {
-        //GIVEN
-        NewPackingItemDto emptyItem = NewPackingItemDto.builder()
-                .build();
-        //WHEN//THEN
-        assertThrows(IllegalArgumentException.class, () -> packingListService.generateNewItem(emptyItem));
-    }
-
-    @Test
-    void deleteItemFromPackingList_whenIdOfListAndItemAreValid() {
-        //GIVEN
-        when(packingListRepository.findById("1")).thenReturn(Optional.ofNullable(packingListWithTwoItems));
-        when(packingListRepository.save(packingListWithOneItem)).thenReturn(PackingList.builder()
-                .id("01")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(PackingItem.builder()
-                        .id("01")
-                        .name("passport")
-                        .build()))
-                .build());
-        //WHEN
-        assertNotNull(packingListWithOneItem);
-        PackingList actual = packingListService.deleteItemFromPackingList(listId, "02");
-        //THEN
-        verify(packingListRepository).save(packingListWithOneItem);
-        PackingList expected = PackingList.builder()
-                .id("01")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(PackingItem.builder()
-                        .id("01")
-                        .name("passport")
-                        .build()))
-                .build();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void deleteItemFromPackingList_whenIdOfListIsNotValid_ShouldThrowNoSuchElementException() {
-        //GIVEN
-        when(packingListRepository.findById(listId)).thenReturn(Optional.empty());
-        //WHEN //THEN
-        assertThrows(NoSuchElementException.class, () -> packingListService.deleteItemFromPackingList(listId, itemId));
-    }
-
-    @Test
-    void deleteItemFromPackingList_whenListOfItemsIsNull_ShouldThrowNoSuchElementException() {
-        //GIVEN
-        PackingList packingList = PackingList.builder()
-                .id("2")
-                .destination("Tokyo")
-                .dateOfArrival(LocalDate.parse("2022-10-03"))
-                .build();
-        when(packingListRepository.findById(listId)).thenReturn(Optional.ofNullable(packingList));
-        //WHEN //THEN
-        assertThrows(NoSuchElementException.class, () -> packingListService.deleteItemFromPackingList("2", itemId));
-    }
-
-    @Test
-    void updatePackingItem_whenIdOfListAndItemIsValid_ShouldReturnUpdatedList() {
-        //GIVEN
-        when(packingListRepository.findById(listId)).thenReturn(Optional.of(packingListWithOneItem));
-        PackingList updatedPackingList = PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(
-                        PackingItem.builder()
-                                .id("01")
-                                .name("swimwear")
-                                .build()))
-                .build();
-        when(packingListRepository.save(updatedPackingList)).thenReturn(PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(
-                        PackingItem.builder()
-                                .id("01")
-                                .name("swimwear")
-                                .build()))
-                .build());
-        //WHEN
-        PackingList actual = packingListService.updatePackingItem(listId, itemId, newPackingItemDto);
-        //THEN
-        PackingList expected = PackingList.builder()
-                .id("1")
-                .destination("Kyoto")
-                .dateOfArrival(LocalDate.parse("2022-09-03"))
-                .packingItemList(List.of(
-                        PackingItem.builder()
-                                .id("01")
-                                .name("swimwear")
-                                .build()))
-                .build();
-        assertEquals(expected, actual);
-        verify(packingListRepository).findById(listId);
-        verify(packingListRepository).save(updatedPackingList);
-    }
-
-    @Test
-    void updatePackingItem_whenIdOfItemIsNotValid_ShouldThrowNoSuchElementException() {
-        //GIVEN
-        when(packingListRepository.findById(listId)).thenReturn(Optional.of(packingListWithOneItem));
-        //WHEN //THEN
-        assertThrows(NoSuchElementException.class, () -> packingListService.updatePackingItem(listId, "02", newPackingItemDto));
-        verify(packingListRepository).findById(listId);
-    }
-
-    @Test
-    void updatePackingItem_whenNameOfItemIsNotGiven_shouldThrowIllegalArgumentException() {
-        //GIVEN
-        NewPackingItemDto emptyItem = NewPackingItemDto.builder()
-                .build();
-        //WHEN //THEN
-        assertThrows(IllegalArgumentException.class, () -> packingListService.updatePackingItem(listId, itemId, emptyItem));
-    }
 }
