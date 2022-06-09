8b3c74b2e35c95c186189ab299adad194d076030
Maria Kohnen <maria.kohnen@gmail.com>
2022-06-09 18:32:59 +0200

    #27 setup login with basic Auth and argon2Encoder. Added all layers and DB for appUsers

---
+++ backend/src/main/java/com/github/mariakohnen/packbag/backend/security/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.github.mariakohnen.packbag.backend.security.config;
+
+import com.github.mariakohnen.packbag.backend.security.service.AppUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserDetailsService appUserDetailsService;
+
+    public SecurityConfig(AppUserDetailsService appUserDetailsService) {
+        this.appUserDetailsService = appUserDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
